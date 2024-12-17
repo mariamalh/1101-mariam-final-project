bd8cd6c09c90e8e050fa88a0f2df1680c6bbff8c
mariamalh <alhroob.mariam@gmail.com>
2024-12-17 18:09:04 +0300

    Update footer.tsx

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -4,6 +4,27 @@ import { EXAMPLE_PATH } from "@/lib/constants";
 export function Footer() {
   return (
     <footer
+      className="bg-neutral-50 border-t border-neutral-200 dark:bg-slate-800">
+      <Container>
+        <div className="py-28 flex flex-col lg:flex-row items-center">
+          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+          
+          </h3>
+          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+            <a
+              href="https://nextjs.org/docs/app/building-your-application/routing/layouts-and-templates"
+            
+   
+            </a>
+            <a
+              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+              className="mx-3 font-bold hover:underline"
+            >
+        
+            </a>
+          </div>
+        </div>
+      </Container>
     </footer>
   );
 }
