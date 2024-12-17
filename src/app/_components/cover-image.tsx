acb36e5a46dd0c1ce4b49c5369ffbadac6c54fa9
mariamalh <alhroob.mariam@gmail.com>
2024-12-17 14:04:25 +0300

    Update cover-image.tsx

--- src/app/_components/cover-image.tsx
+++ src/app/_components/cover-image.tsx
@@ -1,6 +1,6 @@
 import cn from "classnames";
 import Link from "next/link";
-import Image from "next/image";
+import Image from "https://i.imghippo.com/files/uCS5939XmY.webp";
 
 type Props = {
   title: string;
